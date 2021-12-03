35e4e0224ed3a3c6d0fc71d4458bd035be5cd07d
Louis Von Richter <louisvonrichter@gmail.com>
2021-12-03 11:00:19 +1000

    Updated code to work with bullseye workaround

--- vision.py
+++ vision.py
@@ -41,13 +41,13 @@ def threshold():
         frame = frame & mask_rgb
 
         # find contours
-        contours, hierarchy = cv2.findContours(
+        (__, cnts, __) = cv2.findContours(
             mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
         )
 
         # draw one box around the biggest contour
-        if len(contours) > 0:
-            c = max(contours, key=cv2.contourArea)
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             # place circle in centre of box
@@ -75,9 +75,8 @@ def threshold():
 
 threshold()
 
-
-# from picamera.array import PiRGBArray
 # from picamera import PiCamera
+# from picamera.array import PiRGBArray
 # import time
 # import cv2
 
@@ -89,12 +88,15 @@ threshold()
 # time.sleep(2)
 
 # # grab an image from the camera
-# camera.capture(rawCapture, format="bgr")
-# image = rawCapture.array
+# while(True):
+#     rawCapture = PiRGBArray(camera)
 
-# # Convert to a normal RGB space
-# im = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     camera.capture(rawCapture, format="bgr")
+#     image = rawCapture.array
 
-# # display the image on screen and wait for a keypress
-# cv2.imshow("Image", im)
-# cv2.waitKey(0)
+#     # Convert to a normal RGB space
+#     im = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+#     # display the image on screen and wait for a keypress
+#     cv2.imshow("Image", im)
+#     cv2.waitKey(0)
